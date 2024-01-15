ea6befd61968bd366de2c21a547249b58d40c993
lth0422 <117874932+lth0422@users.noreply.github.com>
2024-01-16 01:05:18 +0900

    포인트 빼고 계산 문제 기능 구현 및 주석 달기

--- math_question.py
+++ math_question.py
@@ -1,6 +1,5 @@
 import random
 import time
-import threading
 
 def generate_math_question():
     # Types of operations
@@ -11,7 +10,7 @@ def generate_math_question():
     num1 = random.randint(1, 10)
     num2 = random.randint(1, 10)
 
-    # Creating the question
+    # 나눗셈 무조건 자연수로 답 나오게 num1을 num2의 배수로 설정
     if operation == '/':
         num1 = num2 * random.randint(1, 10)
         question = f"{num1} {operation} {num2}"
@@ -24,7 +23,7 @@ def generate_math_question():
     return question, answer
 
 
-
+# 계산 문제를 출력하고 사용자가 입력한 답을 판별하는 함수
 def execute_math_question(question, answer):
 
     print(f"\r\n문제: {question}= ?\n")
@@ -37,13 +36,23 @@ def execute_math_question(question, answer):
     end_time = time.time()  # 사용자가 대답한 시간 기록
     elapsed_time = end_time - start_time  # 경과 시간 계산
 
+    # 사용자의 답이 맞고 시간도 지켰을 때
     if user_answer == answer and elapsed_time <= 3:
-        print("\n정답입니다!")
-    elif elapsed_time > 3:
-        print("\n시간 초과입니다.")
-    else:
+        print("\n정답입니다^^!")
+
+    # 답은 맞았으나 시간이 초과
+    elif user_answer == answer and elapsed_time > 3:
+        print("\n답은 맞았는데.. 시간 초과입니다.")
+
+    # 시간은 지켰는데 답은 틀림
+    elif user_answer != answer and elapsed_time <= 3:
         print("\n답 틀렸어요 병신아")
 
+    # 답도 틀리고 시간도 못지킴
+    else:
+        print("\n진짜 넌 개병신년이다")
+
+    # 소요 시간 출력, 소숫점 3째자리에서 반올림
     print(f"소요시간: {round(elapsed_time, 2)} 초")
 
 
@@ -52,6 +61,7 @@ def execute_math_question(question, answer):
 print("5개의 계산 문제가 출력됩니다. 제한 시간은 3초입니다. 행운을 빕니다.\n")
 time.sleep(2)
 
+# 5개의 문제가 for 루프를 통해 출력됨
 for _ in range(5):
     q, a = generate_math_question()
     execute_math_question(q, a)
